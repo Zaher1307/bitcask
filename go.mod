8fd5ff0a0420a09fefb269606f5e009dbe90b3a9
zaher1307 <ahmedzaher1307@gmail.com>
2022-11-15 19:47:56 +0200

    add resp server lib to run over bitcask

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/zaher1307/bitcask
 
 go 1.19
 
-require github.com/gofrs/flock v0.8.1
+require (
+	github.com/gofrs/flock v0.8.1
+	github.com/tidwall/resp v0.1.1
+)
 
 require (
 	golang.org/x/sys v0.2.0 // indirect
---
+++ pkg/respserver/resp_server.go
@@ -0,0 +1,63 @@
+package respserver
+
+import (
+	"errors"
+	"log"
+
+	"github.com/tidwall/resp"
+	"github.com/zaher1307/bitcask/pkg/bitcask"
+)
+
+func StartServer() error {
+	bitcask, err := bitcask.Open("./resp_server", bitcask.ReadWrite)
+	if err != nil {
+		return err
+	}
+
+	s := resp.NewServer()
+
+	s.HandleFunc("set", func(conn *resp.Conn, args []resp.Value) bool {
+		if len(args) != 3 {
+			conn.WriteError(errors.New("ERR wrong number of arguments for 'set' command"))
+		} else {
+			err = bitcask.Put(args[1].String(), args[2].String())
+			if err != nil {
+				conn.WriteError(errors.New("ERR cannot set key to value in this store"))
+			}
+			conn.WriteSimpleString("OK")
+		}
+		return true
+	})
+
+	s.HandleFunc("get", func(conn *resp.Conn, args []resp.Value) bool {
+		if len(args) != 2 {
+			conn.WriteError(errors.New("ERR wrong number of arguments for 'get' command"))
+		} else {
+			s, err := bitcask.Get(args[1].String())
+			if err != nil {
+				conn.WriteNull()
+			} else {
+				conn.WriteString(s)
+			}
+		}
+		return true
+	})
+
+	s.HandleFunc("delete", func(conn *resp.Conn, args []resp.Value) bool {
+		if len(args) != 2 {
+			conn.WriteError(errors.New("ERR wrong number of arguments for 'get' command"))
+		} else {
+			err := bitcask.Delete(args[1].String())
+			if err != nil {
+				conn.WriteError(errors.New("ERR cannot delete this item"))
+			} else {
+				conn.WriteSimpleString("OK")
+			}
+		}
+		return true
+	})
+	if err := s.ListenAndServe(":6379"); err != nil {
+		log.Fatal(err)
+	}
+	return nil
+}
